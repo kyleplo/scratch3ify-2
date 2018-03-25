506ef689c4ad61718ed75fe31aae4916401f9357
kyleplo <31634240+kyleplo@users.noreply.github.com>
2018-03-25 13:20:39 -0400

    Create template.js

---
+++ template.js
@@ -0,0 +1,17 @@
+var id;
+var link = "<id>";
+var type = "";
+if(window.data){
+if(window.data.project.id){
+link = link.replace("<id>",data.project.id);
+location.href = link;
+}else if(location.href.startsWith("https://scratch.mit.edu/projects/")){
+document.body.style.background = "white";
+document.body.innerHTML = "<p style='font-family: Arial;color: red'>Scratch3ify 2: An error occured - please contact <a href='https://scratch.mit.edu/users/kyleplo'>@kyleplo</a> with the error code: <b>l" + type + btoa(location.pathname) + "</b></p>";
+}else{
+document.body.style.background = "white";
+document.body.innerHTML = "<p style='font-family: Arial;color: red'>Scratch3ify 2: This doesn't look like a project page - if you think this is an error, please contact <a href='https://scratch.mit.edu/users/kyleplo'>@kyleplo</a> with the error code: <b>q" + type + btoa(location.pathname) + "</b></p>";
+}}else{
+document.body.style.background = "white";
+document.body.innerHTML = "<p style='font-family: Arial;color: red'>Scratch3ify 2: This doesn't look like a project page - if you think this is an error, please contact <a href='https://scratch.mit.edu/users/kyleplo'>@kyleplo</a> with the error code: <b>r" + type + btoa(location.pathname) + "</b></p>";
+}
